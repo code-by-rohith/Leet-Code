680d0798f5e194faabdd8096bb47b98af2e7f4f8
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-02 15:54:04 +0530

    add

---
+++ Easy/Chunk Array.py
@@ -0,0 +1,16 @@
+def chunk_array(arr, size):
+    chunked_array = []
+    for i in range(0, len(arr), size):
+        chunked_array.append(arr[i:i+size])
+    return chunked_array
+
+
+
+arr2 = [1, 9, 6, 3, 2]
+size2 = 3
+print(chunk_array(arr2, size2))
+
+
+
+
+
