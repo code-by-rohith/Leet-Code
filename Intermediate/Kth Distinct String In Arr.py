f8486190f71ad60ffdb0f0dba13c8c17830a6956
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-05 17:41:53 +0530

    add

--- Intermediate/Kth Distinct String In Arr.py
+++ Intermediate/Kth Distinct String In Arr.py
@@ -1,3 +1,20 @@
+"""
+## A distinct string is a string that is present only once in an array.
+
+## Given an array of strings arr, and an integer k, return the kth distinct string present in arr. If there are fewer than k distinct strings, return an empty string "".
+
+## Note that the strings are considered in the order in which they appear in the array.
+
+## Example 1:
+
+## Input: arr =  ["d","b","c","b","c","a"], k = 2
+## Output: "a"
+## Explanation:
+## The only distinct strings in arr are "d" and "a".
+## "d" appears 1st, so it is the 1st distinct string.
+## "a" appears 2nd, so it is the 2nd distinct string.
+## Since k == 2, "a" is returned.
+"""
 from typing import List
 
 class Solution:
