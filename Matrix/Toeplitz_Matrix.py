487f096b998c408e8e32fa16751a49c519abeecf
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-06 21:08:14 +0530

    Add

--- Matrix/Toeplitz_Matrix.py
+++ Matrix/Toeplitz_Matrix.py
@@ -6,7 +6,6 @@ class Solution:
                 if matrix[i][j] != matrix[i + 1][j + 1]:
                     return False
         return True
-
 matrix =[[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]
 obj=Solution()
 print(obj.isToeplitzMatrix(matrix))
