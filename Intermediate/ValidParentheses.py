c00f862b962caafa64e048d741b352b22514628f
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-08 16:28:49 +0530

    add

---
+++ Intermediate/ValidParentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if char == ')' and stack[-1] != '(':
+                    return False
+                if char == '}' and stack[-1] != '{':
+                    return False
+                if char == ']' and stack[-1] != '[':
+                    return False
+                stack.pop()
+        return not stack
+s="()[]{}"
+obj=Solution()
+print(obj.isValid(s))
