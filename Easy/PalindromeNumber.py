d3e10a41d731d88598aaf2e6a2a4c70a5a887fce
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-09 14:12:47 +0530

    add

---
+++ Easy/PalindromeNumber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        rev = 0
+        temp = x
+        while temp:
+            rev = rev*10 + temp%10
+            temp //= 10
+        if x == rev:
+            return True
+        else:
+            return False
+
+x=5321235
+obj=Solution()
+print(obj.isPalindrome(x))
