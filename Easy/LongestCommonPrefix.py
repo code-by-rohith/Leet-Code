560c624491fbe54997d32fe1ddc3fb53ff3e68e5
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-09 16:46:11 +0530

    add

---
+++ Easy/LongestCommonPrefix.py
@@ -0,0 +1,19 @@
+#Longest Common Prefix
+
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+
+strs =  ["flower","flower","floight"]
+obj=Solution()
+print(obj.longestCommonPrefix(strs))
