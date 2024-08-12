bafa70f7bf170768a585b6262665ddb942628c88
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-12 20:57:15 +0530

    add

--- Easy/first-occurrence-in-a-string.py
+++ Easy/first-occurrence-in-a-string.py
@@ -7,7 +7,7 @@
 
 class Solution(object):
     def strStr(self, haystack, needle):
-        for i in range(len(haystack) - len(needle) + 1):
+        for i in range(len(haystack) ):
             if haystack[i : i+len(needle)] == needle:
                 return i
         return -1
