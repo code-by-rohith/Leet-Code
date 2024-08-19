3cbdcfa61ea49a6d7c72d16ac7addf54fd74cde2
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-19 22:47:28 +0530

    add

---
+++ Easy/2 Keys Keyboard.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        steps = 0
+        factor = 2
+        while n > 1:
+            while n % factor == 0:
+                steps += factor
+                n //= factor
+            factor += 1
+        return steps
+
+obj=Solution()
+print(obj.minSteps(50))
