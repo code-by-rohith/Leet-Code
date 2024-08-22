cbd8ea634f4a02d65269c6e823fb19eefa2fb36a
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-08-22 23:22:25 +0530

    add

---
+++ Easy/mergeSortedArray.py
@@ -0,0 +1,28 @@
+def merge(nums1, m, nums2, n):
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
+
+
+nums1 = [1,0,0,0]
+m = 1
+nums2 = [2, 5, 6]
+n = 3
+merge(nums1, m, nums2, n)
+print(nums1)
+
