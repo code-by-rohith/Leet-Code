9b9a18586636e08d0e47266b1a3ed89b599d3757
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-02 21:03:49 +0530

    add

---
+++ Easy/Convert 1D To 2D.py
@@ -0,0 +1,15 @@
+import numpy as np
+from typing import List
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if len(original) != m * n:
+            return []
+        temp = np.array(original)
+        newarr = temp.reshape(m, n)
+
+        return newarr.tolist()
+
+original=[5,8,6,4,2]
+obj =Solution()
+print(obj.construct2DArray(original,5,1))
