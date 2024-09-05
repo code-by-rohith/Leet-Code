c629b1322823eb2b5655bc1468d1798c63b85664
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-05 21:31:15 +0530

    add

--- DynamicProgramming/Pascals Triangle.py
+++ DynamicProgramming/Pascals Triangle.py
@@ -1,14 +1,19 @@
 from typing import List
 
 def generate(numRows: int) -> List[List[int]]:
-    a = []
-    for i in range(numRows):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = a[i - 1][j - 1] + a[i - 1][j]
-        a.append(row)
-    return a
+    res = [[1]]  # Initialize the result with the first row
+
+    for i in range(numRows - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+
+    return res
+
 
 # Test cases
 print(generate(5))
-print(generate(3))
+print(generate(4))
+
