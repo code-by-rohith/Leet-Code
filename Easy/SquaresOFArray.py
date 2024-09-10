4f2ab22771146acf9e0ff7fcb8d330fb0a0bf047
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-10 12:08:00 +0530

    add

--- Easy/SquaresOFArray.py
+++ Easy/SquaresOFArray.py
@@ -1,16 +1,19 @@
-def sortedSquares(nums):
-    left = 0
-    right = len(nums) - 1
-    result = [0] * len(nums)
-    index = len(nums) - 1
+def sorted_squares(nums):
+    n = len(nums)
+    result = [0] * n
+    left, right = 0, n - 1
+    index = n - 1
     while left <= right:
-        if abs(nums[left]) > abs(nums[right]):
-            result[index] = nums[left] ** 2
+        left_square = nums[left] ** 2
+        right_square = nums[right] ** 2
+        if left_square > right_square:
+            result[index] = left_square
             left += 1
         else:
-            result[index] = nums[right] ** 2
+            result[index] = right_square
             right -= 1
         index -= 1
     return result
 
-print(sortedSquares(nums=[-5,-2,0,1,2,8,9,17]))
+
+print(sorted_squares(nums=[-5,-2,0,1,2,8,9,17]))
