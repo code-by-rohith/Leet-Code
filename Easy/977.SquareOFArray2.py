f15d11f0ee5b6ed6d359eacd29b44221e4d32c43
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-11 16:14:10 +0530

    add

---
+++ Easy/977.SquareOFArray2.py
@@ -0,0 +1,26 @@
+def main(arr):
+    res=[]
+    left=0
+    right=len(arr)-1
+    while left <=right:
+        if arr[left]**2 > arr[right]**2:
+            res.append(arr[left]**2)
+            left+=1
+        else:
+            res.append(arr[right]**2)
+            right -=1
+    return res[::-1]
+
+
+arr = [-3, -2, 0, 1, 2, 3, 4, 5]
+print(main(arr))
+
+
+
+
+
+
+
+
+
+
---
+++ Easy/Perfect Number.py
@@ -0,0 +1,13 @@
+def main(num):
+    if num <= 1:
+        return  False
+    sum_div=1
+    for i in range(2,int(num**0.5)+1):
+        if num%i ==0:
+            sum_div +=i +num//i
+    return sum_div == num
+
+
+
+nums=28
+print(main(nums))
