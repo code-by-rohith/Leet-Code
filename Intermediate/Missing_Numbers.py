f3b08b21fece151d29e461480401651486f183f6
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-25 20:37:48 +0530

    add

---
+++ Intermediate/Missing_Numbers.py
@@ -0,0 +1,27 @@
+
+def method1(nums):
+
+        n = len(nums)
+        ans = 0
+        for i in range(1, n + 1):
+            ans ^= i
+        for num in nums:
+            ans ^= num
+        return ans
+nums=[0,1,2,3,4,5]
+print(method1(nums))
+
+num=[0,1,2,3,4,5]
+def main(num):
+    n=len(num)
+    temp=0
+    samp=0
+    for i in range(1,n+1):
+        temp += i
+    for j in num:
+        samp += j
+    return temp - samp
+
+print(main(num))
+
+
