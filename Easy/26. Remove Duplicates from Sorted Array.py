a36d34a862bb0e02309a3545d0968a4e37f721de
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-10-16 17:39:36 +0530

    adfd

---
+++ Easy/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,10 @@
+nums = [1,2,3,4,5]
+def main(nums):
+    j = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[j] = nums[i]
+            j += 1
+    return j
+
+print(main(nums))
