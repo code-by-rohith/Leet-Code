fb57c22dddb67023f20f0e94c0bd54def12bd0e5
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-11-15 01:09:38 +0530

    first commit

---
+++ Graph/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root) -> int:
+        if root is None:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return 1 + max(left_depth, right_depth)
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+solution = Solution()
+result = solution.maxDepth(root)
+print(result)
