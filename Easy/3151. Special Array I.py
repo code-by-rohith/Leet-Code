9ad90697467438aaff3a8c7f775ca9fbb0e7c093
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-11-28 00:09:36 +0530

    first commit

---
+++ Easy/3151. Special Array I.py
@@ -0,0 +1,12 @@
+
+def main(nums):
+    if len(nums) == 1:
+        return True
+
+    for i in range(len(nums) - 1):
+        if nums[i] & 1 == nums[i + 1] & 1:
+            return False
+
+    return True
+
+print(main(nums = [1,2,2,2,2,3]))
